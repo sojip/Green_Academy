1e642d2c5ba7e033a37945b5dd3ca5addec9687d
sojip <sojipquantin@gmail.com>
2020-04-14 17:14:40 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def acceuil():
+    courses_titles = ["Title 1", "Title2", "Title3"]
+    categories = ["Marketing" , "Social Network", "Entrepreneurship", "Subject N° ...", "Subject N° ..", "Subject N° .."]
+    return render_template("accueil.html", courses_titles = courses_titles, categories = categories)
+
+if __name__ == "__main__":
+    app.run(debug=True)
